0bd395875b031974e257816a333141485c69d2e3
Your Name <you@example.com>
2022-10-20 22:18:32 +0530

    first-push

---
+++ method2.java
@@ -0,0 +1,16 @@
+class method2{
+  public static void main(String[] args){
+   completeBEcourse();
+   }
+   static void completeBEcourse(){
+       completeDiplomacourse();
+	   System.out.println("I completed BE course");
+	   }
+     static void completeDiplomacourse(){
+     completeSSLC();
+	 System.out.println("I completed diploma");
+	 }
+	static void completeSSLC(){
+	  System.out.println("I completed SSLC");
+	  }
+	}
