0dc385d572834bfb163c8db8ce142391a1bdb63f
Your Name <you@example.com>
2022-10-20 22:10:15 +0530

    first-push

---
+++ BRANCHING STATEMENTS/break2.java
@@ -0,0 +1,9 @@
+class break2{
+public static void main(String []args){
+  
+   for(int i=1;i<8;i++){
+      if(i==6)
+	  break;
+   System.out.println("Ram");
+}
+}}
---
+++ BRANCHING STATEMENTS/continue2.java
@@ -0,0 +1,11 @@
+class continue2{
+public static void main(String[] args){
+   int n=10;
+   for(int i=1;i<=n;i++){
+    if(i==4)
+	continue;
+	System.out.println("******");
+	}
+	
+}}
+	
